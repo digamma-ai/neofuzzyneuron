e633fe8a71927b39ca96b11ec11bc546b1f9e8c1
Dmitrii Levin <dlevin@codeminders.com>
2017-06-27 18:05:35 +0300

    tutorial update

--- nfn.py
+++ nfn.py
@@ -1,153 +1,270 @@
 import numpy as np
-from .mfunc import *
+
+from sklearn.linear_model import Ridge
+from copy import deepcopy
+
+
+def rollwin(x, window, stride = 1):
+    """
+    Transforms a 1D-array into rolling window samples
+
+    Uses fast numpy shape manipulations and performs in O(1) time.
+
+    Args
+        x (ndarray): input array
+        window (int): window size
+        stride (int): window step
+
+    Returns
+        ndarray: 2D-array of shape ``(x.size - window, window)``, 
+            where rows as consequitive samples from the array ``x``
+
+    Raises
+        ValueError: if input array is shorted than ``window``
+    """
+
+    if x.size < window:
+        raise ValueError("Array must containt at least %i values" % window)
+    dsize   = x.dtype.itemsize
+    strides = (stride*dsize, dsize)
+    shape   = ((x.size - window)//stride + 1, window)
+    return np.lib.stride_tricks.as_strided(x, strides = strides, shape = shape)
+
+
+def uniform_grid(X, n_rules):
+    """
+    Generates a uniform grid from a set of input variables
+
+    Args
+        X (ndarray): row-based data matrix
+        n_rules (int): number of fuzzy inference rules;
+            the resulting grid has ``n_rules + 3`` points
+
+    Returns
+        ndarray: 2D-array of grid points per input variable
+
+    Raises
+        ValueError: if ``n_rules < 1``
+    """
+
+    if n_rules < 1:
+        raise ValueError("Number of inference rules must be > 0")
+    
+    x_min = X.min(axis = 0)
+    x_max = X.max(axis = 0)     
+    step = (x_max - x_min) / n_rules
+   
+    grid = np.vstack(np.linspace(a - h, b + h, n_rules + 2) 
+                     for a, b, h in zip(x_min, x_max, step))
+    return grid.T
+
+
+def density_grid(X, n_rules): 
+    """
+    Generates a density-based grid from a set of input variables
+
+    Grid per varible is build as a set of ``n_rules + 3`` quantiles
+
+    Args
+        X (ndarray): row-based data matrix
+        n_rules (int): number of fuzzy inference rules
+
+    Returns
+        ndarray: 2D-array of grid points per input variable
+
+    Raises
+        ValueError: if ``n_rules < 1``
+    """
+
+    if n_rules < 1:
+        raise ValueError("Number of inference rules must be > 0")
+
+    grid = list()
+    for i in range(X.shape[1]):
+        quantiles = np.percentile(X[:,i], q = np.linspace(0, 100, n_rules))
+        pad_left  = 2*quantiles[ 0] - quantiles[ 1]
+        pad_right = 2*quantiles[-1] - quantiles[-2]        
+        grid.append(np.hstack([pad_left, quantiles, pad_right]))
+    grid = np.vstack(grid).T
+    return grid  
+
+
+def get_sym_mfunc(grid):
+    """
+    Creates symmetric triangular membership functions
+    for a uniform grid
+
+    Resulting function accepts batches of data. 
+    Faster than asymmetric functions
+
+    Args
+        grid (ndarray): uniform fuzzy partioning grid 
+
+    Returns
+        function: a batch processing function
+    """
+
+    _grid = grid.copy()
+
+    a = _grid[None,  :-2, :]
+    b = _grid[None, 1:-1, :]
+    
+    p = -1/(b - a)
+    q = b
+    
+    def _mfunc(batch):          
+        X = batch[:,None,:]
+        M = np.maximum(0, p*np.abs(X - q) + 1)
+        return M
+    
+    return _mfunc
+
+
+def get_asym_mfunc(grid):
+    """
+    Creates asymmetric triangular membership functions
+    for a density-based grid
+
+    Resulting function accepts batches of data. 
+
+    Args
+        grid (ndarray): non-uniform fuzzy partioning grid 
+
+    Returns
+        function: a batch processing function
+    """
+
+    _grid = grid.copy()
+
+    a = _grid[None,  :-2, :]
+    b = _grid[None, 1:-1, :]
+    c = _grid[None, 2:  , :]
+    
+    p =  1 / (b - a)
+    q = -a / (b - a)
+    r = -1 / (c - b)
+    s =  c / (c - b)
+    
+    def _mfunc(batch):
+        X = batch[:,None,:]
+        lmask = np.ravel((X >= a) & (X < b))
+        rmask = np.ravel((X >= b) & (X < c))
+        M = np.zeros(rmask.size)
+        M[lmask] = np.ravel(p*X + q)[lmask]
+        M[rmask] = np.ravel(r*X + s)[rmask]
+        M = M.reshape((X.shape[0], a.shape[1], X.shape[2]))
+        return M
+    
+    return _mfunc
+
+
+def data_initializer(X, y, grid):
+    """
+    Returns NFN weights initialized by averages over each fuzzy partition
+
+    Args: 
+        X (2d ndarray): Input sample
+        y (1d ndarray): target values for X
+        grid (2d ndarray): fuzzy partitioning grid
+
+    Returns:
+        2d ndarray: initialized NFN weights
+    """
+
+    _, n_inputs = X.shape
+    a = grid[ :-2,:]
+    b = grid[2:  ,:]
+    mask = (X[:,None,:] >= a[None,:,:]) & (X[:,None,:] <  b[None,:,:])
+    W = np.sum(y[:, None, None] * mask, axis = 0) 
+    W /= n_inputs * np.maximum(np.sum(mask, axis = 0), 1)
+    return W
+
+
+def dynamic_predict(model, init, n_steps):
+    """
+    Performs a dynamice out-of-sample prediction of a time series
+
+    Args:
+        model (object): a prediction model; must implement `predict` method
+        init (ndarray): initial sample
+        n_steps (int): number of prediction steps
+
+    Returns:
+        ndarray: prediction including initial values
+    """
+
+    n = len(init)
+    y_dynamic = init.tolist()
+    for i in range(n_steps):
+        x = np.array(y_dynamic[-n:]).reshape(1,-1)
+        y = model.predict(x)
+        y_dynamic.append(y)
+    return np.array(y_dynamic)
+
+
+def check_solver(solver):
+    try:
+        solver.fit
+        solver.predict
+    except:
+        raise ValueError("Solver does not implement `fit` or `predict` method, or `coef_` field")
 
 
 class NeoFuzzyNeuron:  
     """
     Neo-fuzzy neuron model
 
-    Supports uniform and quantile grids, linear membership functions and
-    full batch RPROP training with early stopping.
+    Uses sklearn's `Ridge(alpha = 1e-4)` model as a solver. Supports
+    uniform and quantile partitioning as well as custom grids
 
-    Attributes
-        n_rules (int): number of fuzzy inference rules in the neuron
-        uniform (bool): if True, the uniform grid of fuzzy partitions is used;
-            if False, the input domain is partitioned by quantiles of the 
-            unconditional time series distribution. Uniform grid is makes evaluation
-            and training ~4.0x faster.
-        n_epochs (int): number of training iteration
-        max_no_best (int): early stopping parameter
-        tol (float): optimization precision
-        write_log (bool): if True, training process (train/test MSE, time per epoch)
-            is written into a ``log_`` field
-    """
+    Attributes:
+
+    """    
     
-    def __init__(self, n_rules, uniform = True, 
-                 n_epochs = 1000, max_no_best = np.inf, tol = 1e-8, write_log = True):   
-        self.n_rules  = n_rules       
-        self.n_epochs = n_epochs
-        self.max_no_best = max_no_best
-        self.tol = tol
-        self.uniform = uniform
-        
-        self._weights = None
-        self._write_log = write_log       
-        self._mse = 0.0        
-        
-        self.log_ = list()
-
-
-    def _data_initializer(self, X, y, grid):
-        _, n_inputs = X.shape
-        a = grid[ :-2,:]
-        b = grid[2:  ,:]
-        mask = np.logical_and(
-            X[:, None, :] >= a[None, :, :],
-            X[:, None, :] <  b[None, :, :]
-        )
-        W = np.sum(y[:, None, None] * mask, axis = 0) 
-        W /= n_inputs * np.maximum(np.sum(mask, axis = 0), 1)
-        return W
-        
-        
-    def _initialize(self, X_train, X_test, y_train, y_test):
-        """
-        Special initialization method for regression tasks. 
-
-        Initializes weights with averages over crisp supports of 
-        corresponding fuzzy partitions. Also sets initial RPROP steps
-        to 1e-4 to ensure smooth convergence from the initial point,
-        as this initialization method has proven to be quite good.
-
-        Args
-            X (ndarray): input data
-            y (ndarray): target variable
-        """
-
-        grid_gen = uniform_grid if self.uniform else density_grid
-        mfunc_gen = get_sym_mfunc if self.uniform else get_asym_mfunc
-        
-        grid = grid_gen(np.vstack([X_train, X_test]), self.n_rules)
-        self._mfunc = mfunc_gen(grid)
-        self._weights = self._data_initializer(X_train, y_train, grid)
-        self._weights_update = np.zeros_like(self._weights)
-        self._step = 1e-4 * np.ones_like(self._weights)
-        self._gradients = np.ones_like(self._weights, dtype = int)
-
-
-    def _predict_batch(self, X, memberships = None):
-        if memberships is None:
-            memberships = self._mfunc(X)
-        weights = self._weights[None,:,:]
-        y = np.sum(weights * memberships, axis = (1,2))
-        return y
-
-
-    def _gradient(self, errors, M):
-        E = errors[:, None, None]   
-        return np.mean(E * M, axis = 0) 
-    
-    
-    def _rprop_iter(self, M_train, y_train, inc = 1.2, dec = 0.5): 
-        y_pred = self._predict_batch(None, M_train)
-        errors = y_train - y_pred       
-        gradients = np.sign(self._gradient(errors, M_train))
-        
-        sign = gradients * self._gradients
-        no_sign_change = sign >= 0
-        sign_change = sign < 0
-        
-        self._step[sign > 0] *= inc
-        self._step[sign_change] *= dec
-        
-        # weights_update = self._weights_update.copy()
-        self._weights_update[no_sign_change] = -self._step[no_sign_change] * gradients[no_sign_change]
-        
-        mse = np.mean(np.square(errors))
-        self._weights[no_sign_change] -= self._weights_update[no_sign_change]       
-        if mse > self._mse:
-            self._weights[sign_change] += self._weights_update[sign_change]
+    def __init__(self, n_rules = None, uniform = True, solver = None, grid = None): 
+        if grid is not None:
+            self.n_rules = grid.shape[0] - 2
+        elif n_rules is not None:
+            self.n_rules = n_rules       
+        else:
+            raise ValueError('Number of rules or grid must be provided')
             
-        # self._weights_update = weights_update.copy() 
-        self._gradients = gradients.copy()
-        self._mse = mse
-        return mse
+        self.uniform = uniform
+        self.weights_ = None
+        self.grid = grid.copy() if grid is not None else None
+        self.mfunc = get_asym_mfunc(grid) if grid is not None else None
+        
+        if solver is not None:
+            check_solver(solver)
+            self.solver_ = deepcopy(solver)
+        else:
+            self.solver_ = Ridge(alpha = 1e-4)      
                    
     
-    def fit(self, X_train, X_test, y_train, y_test): 
-        self._initialize(X_train, X_test, y_train, y_test)
-        
-        best = (-1, np.inf, self._weights)
-        n_no_best = 0 
-        i = 0    
-
-        M_train = self._mfunc(X_train)
-        M_test = self._mfunc(X_test)
-        while i < self.n_epochs or self.n_epochs < 0:           
-            self._rprop_iter(M_train, y_train)            
-            y_test_pred = self._predict_batch(None, M_test)
-            mse_test = np.mean(np.square(y_test - y_test_pred))
-
-            if mse_test < best[1] - self.tol:
-                best = (i, mse_test, self._weights.copy())
-                n_no_best = 0
+    def fit(self, X, y): 
+        if self.grid is None:
+            if self.uniform:
+                grid = uniform_grid(X, self.n_rules)
+                self.mfunc = get_sym_mfunc(grid)
             else:
-                n_no_best += 1     
+                grid = density_grid(X, self.n_rules)
+                self.mfunc = get_asym_mfunc(grid)
+        
+        M = self.mfunc(X)
+        M = M.reshape(M.shape[0], M.shape[1] * M.shape[2])
 
-            if n_no_best > self.max_no_best:
-                self._weights = best[2].copy()
-                if self._write_log:
-                    self.log_.append((i, self._mse, mse_test))
-                break  
-            i += 1
+        self.solver_.fit(M, y)
+        self.weights_ = self.solver_.coef_.copy()
+        self.weights_ = self.weights_.reshape((self.n_rules, X.shape[1]))
+        self.c_ = self.solver_.intercept_
         return self
 
         
     def predict(self, X): 
-        memberships = self._mfunc(X)
-        weights = self._weights[None,:,:]
-        return np.sum(weights * memberships, axis = (1,2))
+        M = self.mfunc(X)
+        weights = self.weights_[None,:,:]
+        return np.sum(weights * M, axis = (1,2)) + self.c_
 
 
-    def fit_predict(self, ts):
-        return self.fit(ts).predict(ts)
+    def fit_predict(self, X, y):
+        return self.fit(X, y).predict(X) 
